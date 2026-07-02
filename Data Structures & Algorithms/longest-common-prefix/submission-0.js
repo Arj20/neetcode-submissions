3d56a4521c557d832784d7435f2c4c010aec6cd0
Arihant Jain <30536355+Arj20@users.noreply.github.com>
2026-07-03 00:37:15 +0530

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+
+        if (strs.length === 0) return "";
+
+        for (let i = 0; i < strs[0].length; i++) {
+
+            const ch = strs[0][i];
+
+            for (let j = 1; j < strs.length; j++) {
+
+                // Current string ended OR mismatch
+                if (
+                    i >= strs[j].length ||
+                    strs[j][i] !== ch
+                ) {
+                    return strs[0].slice(0, i);
+                }
+            }
+        }
+
+        return strs[0];
+    }
+}
