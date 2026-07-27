e4166d9b3443413c49c18e7957e94ba7a1e379b0
Arihant Jain <30536355+Arj20@users.noreply.github.com>
2026-07-27 09:58:08 +0530

    Add: three-integer-sum - submission-10

---
+++ Data Structures & Algorithms/three-integer-sum/submission-10.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        let sortedArr = nums.sort((a,b)=>a-b)
+        let result = [];
+        for(let i=0;i<sortedArr.length-2;i++){
+            if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+            let left = i+1;
+            let right = sortedArr.length-1;
+           
+            while(left<right){
+                 let sum = sortedArr[i] + sortedArr[left] + sortedArr[right];
+                if(sum<0){
+                    left++;
+                }
+                else if(sum>0){
+                    right--;
+                }
+                else{
+                    result.push([sortedArr[i],sortedArr[left],sortedArr[right]])
+                    left++;
+                    right--;
+                    while(left<right && sortedArr[left]===sortedArr[left-1]){
+                        left++;
+                    }
+                     while(left<right && sortedArr[right]===sortedArr[right+1]){
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
